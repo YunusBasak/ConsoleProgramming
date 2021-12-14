12eba559485a9234a3a5bf9540e5a57acc8875b8
YunusBasak <69273379+YunusBasak@users.noreply.github.com>
2021-12-14 22:13:29 +0300

    Program.cs

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleProgramlama
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("İsminizi Giriniz:");
+            string name = Console.ReadLine();
+            Console.WriteLine("soyadınızı Giriniz:");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Merhaba! " + name + " " + surname);
+        }
+    }
+}
